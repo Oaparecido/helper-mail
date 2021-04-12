e73d856f86f504a9685263d7dd723be0fed4228f
Oaparecido <daniel.aparecido4683@gmail.com>
2021-04-12 14:37:51 -0300

    ✨ translate file HTML

--- src/Services/Mailer/ActionsMailing.php
+++ src/Services/Mailer/ActionsMailing.php
@@ -4,7 +4,35 @@
 namespace HelperMail\Services\Mailer;
 
 
-class ActionsMailing
-{
+use HelperMail\Services\AmazonSES;
 
+class ActionsMailing extends AmazonSES
+{
+    protected string $templateFile = 'actions';
+    protected string $translateKey = 'actions-mailing';
+
+    /**
+     * Function to send e-mail for actions approved by administrator
+     * @param $toName
+     * @param $toEmail
+     * @param $link
+     * @param $action_title
+     * @param $entity
+     * @return string
+     */
+    public function actionApproved($toName, $toEmail, $link, $action_title)
+    {
+        $untranslatable = ['name' => $toName, 'link' => $link];
+        $this->translatable  = [
+            'greeting',
+            'message_approve_default' => ['action_title' => $action_title],
+            'message_invite_volunteers',
+            'message_attention_notification',
+            'cta',
+            'button_label'
+        ];
+//        $this->entity_artisan = $entity;
+
+        return $this->dispatcher($toEmail, $toName, __FUNCTION__, $untranslatable);
+    }
 }
