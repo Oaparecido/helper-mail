e5308c03802f2e8dbdf4d7ccfdb1890241b5f636
Oaparecido <daniel.aparecido4683@gmail.com>
2021-04-15 15:51:10 -0300

    ✅ update tests

--- tests/AmazonSESTest.php
+++ tests/AmazonSESTest.php
@@ -5,13 +5,14 @@ use HelperMail\Services\Mailer\ActionsMailing;
 test('View HTML file', function () {
     $settings = [
         'to_name' => 'mongodb name',
-        'to_email' => 'mongodb@gmail.com',
+        'to_email' => 'daniel.aparecido@maquinadobem.com',
         'link' => 'mongodb link',
         'action_title' => 'ação mongodb',
     ];
+
     $mailing = new ActionsMailing();
 
     $mailer = $mailing->actionApproved($settings['to_name'], $settings['to_email'], $settings['link'], $settings['action_title']);
 
-    expect($mailer)->toBe('');
+    expect($mailer)->toBe(['status' => true, 'message' => 'e-mail enviado com sucesso']);
 });
